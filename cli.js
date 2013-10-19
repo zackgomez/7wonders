c096df62b55669a9208e8f7e6c2b31aba230bc8b
Elliot Lynde <elliotlynde@gmail.com>
2013-10-19 13:47:32 -0400

    Consolidate game logic in game.js

--- cli.js
+++ cli.js
@@ -1,29 +1,13 @@
 var Game = require('./game');
 var Deck = require('./deck');
 var invariant = require('./invariant');
+var _ = require('underscore');
 
 console.log('Welcome to the 7 Wonders command line');
 
 var num_players = 4;
 
-var game = new Game(num_players);
+console.log('Starting first age');
+Game.createWithNRandomSelectionBots(4).run().dumpState();
 
-var deck = Deck.forAge(1, num_players);
-
-console.log('Dealing out cards for first age');
-
-for (var i = 0; i < game.players.length; i++) {
-  game.players[i].current_hand = deck.splice(0, 7);
-}
-
-invariant(deck.length == 0, 'All cards should be dealt');
-for (var i = 0; i < game.players.length; i++) {
-  var player = game.players[i];
-  invariant(
-    player.current_hand.length == 7, 
-    'Each player should have seven cards. '+player.name+' has '+
-    player.current_hand.length + ' cards'
-  );
-}
-
-console.log('Cards dealt, this is as far as we are for now');
+console.log('\nJust ran one age without rule checks (not yet implemented)...');
--- game.js
+++ game.js
@@ -3,27 +3,84 @@ var cards = require('./cards');
 var wonders = require('./wonders');
 var invariant = require('./invariant');
 
-var Game = function(num_players) {
+var Game = function(player_funcs) {
+  var num_players = player_funcs.length;
   invariant(num_players >= 3 && num_players <= 7, '3-7 players supported');
 
-  this.age = 1;
-
-  var deck = cards.deck_for_age(1, num_players);
   var selected_wonders = _.sample(wonders, num_players);
 
+  this.discards = [];
   this.players = [];
   for (var i = 0; i < num_players; i++) {
     this.players.push({
       name: 'player'+(i+1),
       wonder: selected_wonders[i],
-      hand: deck.slice(i*7, (i+1)*7),
       money: 3,
       current_hand: [],
       board: [],
-      military_score: 0
+      military_score: 0,
+      play_func: player_funcs[i],
     });
-  
   }
+
+  this.startAge(1);
+};
+
+Game.createWithNRandomSelectionBots = function(num_bots) {
+  return new Game(
+    _(num_bots).times(function () { return random_selection; })
+  );
+};
+
+Game.prototype.run = function() {
+  while (!this.isEndOfAge()) {
+    var passed_cards = _.map(this.players, function(p) {
+      var choice = p.play_func(p);
+      p.board.push(p.current_hand[choice]);
+      p.current_hand.splice(choice, 1);
+      var to_pass = p.current_hand;
+      p.current_hand = [];
+      return to_pass;
+    });
+
+    if (passed_cards[0].length == 1) {
+      _.each(passed_cards, function (cards) {
+        this.discards.push(cards[0]);
+      }.bind(this));
+    } else {
+      console.log('Players picking card, passing '+passed_cards[0].length);
+      for (var i = 0; i < this.players.length; i++) {
+        this.players[i].current_hand = passed_cards[i];
+      }
+    }
+  }
+
+  return this;
+};
+
+var random_selection = function(player) {
+  return 0; // just pick first card
+};
+
+Game.prototype.startAge = function(age_num) {
+  this.age = age_num;
+  var deck = cards.deck_for_age(this.age, this.players.length);
+
+  _.each(this.players, function(p) {
+    p.current_hand = deck.splice(0, Game.HAND_SIZE);
+  });
+
+  _.each(this.players, function (player) {
+    invariant(
+      player.current_hand.length == Game.HAND_SIZE, 
+      'Each player should have seven cards. '+player.name+' has '+
+      player.current_hand.length + ' cards'
+    );
+  });
+};
+
+Game.prototype.isEndOfAge = function() {
+  return this.players[0].current_hand.length == 0;
 };
 
 Game.prototype.resolveChoices = function (choices) {
@@ -42,10 +99,13 @@ Game.prototype.resolveChoices = function (choices) {
 }
 
 Game.prototype.dumpState = function() {
-  console.log(
-    this.age,
-    this.players
-  );
+  console.log("\nCurrent age: "+this.age+"\n");
+  console.log("\nPlayers:");
+  console.log(this.players);
+  console.log("\nDiscard pile: ");
+  console.log(this.discards);
 }
+
+Game.HAND_SIZE = 7;
 
 module.exports = Game;
