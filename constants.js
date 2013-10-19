46b530c545988cf80207b6d0fc2ea947f30273c3
Zack Gomez <zack.gomez@gmail.com>
2013-10-19 16:11:36 -0400

    guilds effects/cost + arena fixes

--- constants.js
+++ constants.js
@@ -1,3 +1,5 @@
+var invariant = require('./invariant');
+
 module.exports = {
   // Wood, Brick, Stone, Ore
   BASIC_RESOURCES: ['W', 'B', 'S', 'O'],
@@ -12,4 +14,17 @@ module.exports = {
   RIGHT: 'RIGHT',
   NEIGHBORS: 'NEIGHBORS',
   ALL: 'ALL',
+
+  assertDirection: function(dir) {
+    // TODO(zack): uhhh clean this 'module.exports' business up
+    invariant(
+      dir === module.exports.SELF ||
+      dir === module.exports.LEFT ||
+      dir === module.exports.RIGHT ||
+      dir === module.exports.NEIGHBORS ||
+      dir === module.exports.ALL,
+      'direction must be self, left, right or neighbors or all.  got: '+dir
+    );
+    return dir;
+  },
 };
--- effects.js
+++ effects.js
@@ -1,3 +1,4 @@
+var _ = require('underscore');
 var constants = require('./constants');
 var invariant = require('./invariant');
 
@@ -9,6 +10,7 @@ module.exports = {
     };
   },
   make_money_for_card_type_effect: function (card_type, direction, amount) {
+    constants.assertDirection(direction);
     return function (game, owning_player) {
       // TODO fill amount based on game, card_type, owning_player and direction
       var total = 0;
@@ -18,20 +20,41 @@ module.exports = {
   play_discarded_card_effect: function (game, owning_player) {
     // TODO
   },
-  make_trading_resource: function (dir, tradable_resources) {
-    invariant(
-      dir === constants.LEFT ||
-      dir === constants.RIGHT ||
-      dir === constants.NEIGHBORS,
-      'direction must be left, right or both'
-    );
-    return function(game_state, owning_player) {
+  make_trading_resource: function (direction, tradable_resources) {
+    constants.assertDirection(direction);
+    return function(game, owning_player) {
       // TODO return available resources/cost
       return [];
     };
   },
   make_vps_for_card_type_effect: function (card_type, direction, amount) {
-    return function(game_state, owning_player) {
+    constants.assertDirection(direction);
+    return function(game, owning_player) {
+      // TODO
+      return 0;
+    };
+  },
+  vps_for_neighbor_military_losses: function (game, owning_player) {
+    // TODO
+    return 0;
+  },
+  make_sum_vps_effect: function (items) {
+    return function(game, owning_player) {
+      return _.reduce(items, function (total, item) {
+          return total + item(game, owning_player);
+        }, 0);
+    };
+  },
+  make_money_for_wonder_stages_effect: function (direction, amount) {
+    constants.assertDirection(direction);
+    return function (game, owning_player) {
+      // TODO
+      return 0;
+    };
+  },
+  make_vps_for_wonder_stages_effect: function (direction, amount) {
+    constants.assertDirection(direction);
+    return function (game, owning_player) {
       // TODO
       return 0;
     };
