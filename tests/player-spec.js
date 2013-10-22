ee5722e2a9faefb0f2caeeb1a1d7df108935431a
Elliot Lynde <elliotlynde@gmail.com>
2013-10-21 21:58:42 -0400

    Small test changes

--- tests/player-spec.js
+++ tests/player-spec.js
@@ -4,10 +4,10 @@ var Helpers = require('./helpers');
 describe('player tests', function () {
   it('should compute military correctly', function() {
     var p = new Player();
-    p.wonder = {stages: [{military: 9}]};
+    p.wonder = {stages: [Helpers.militaryCard(9)]};
     expect(p.getMilitaryStrength()).toBe(0);
 
-    p.board = [ { type: 'military', military : 1 } ];
+    p.board = [ Helpers.militaryCard(1) ];
     expect(p.getMilitaryStrength()).toBe(1);
 
     p.board = [ 
@@ -21,16 +21,28 @@ describe('player tests', function () {
     p.wonder_upgrade_cards.push(Helpers.victoryCard());
     
     expect(p.getMilitaryStrength()).toBe(14);
-  })
+  });
+
+  it('should include built military wonders in strength', function () {
+    var player = new Player();
+
+    player.wonder = {stages: [Helpers.militaryCard(1), Helpers.militaryCard(2)]};
+    player.wonder_upgrade_cards.push(Helpers.victoryCard(0));
+    player.board.push(Helpers.militaryCard(2));
+
+    expect(player.getMilitaryStrength()).toEqual(3); 
+  });
 
   it('should return completed wonder stages', function () {
     var player = new Player();
-    player.wonder = {stages: [{vps: 3}, {vps: 5}]};
+    player.wonder = {stages: [Helpers.victoryCard(3), Helpers.victoryCard(5)]};
     player.wonder_upgrade_cards.push(Helpers.victoryCard(0));
 
-    expect(player.getCompletedWonderStages()).toEqual([{vps: 3}]);
+    expect(player.getCompletedWonderStages()).toEqual([Helpers.victoryCard(3)]);
 
     player.wonder_upgrade_cards.push(Helpers.victoryCard(0));
-    expect(player.getCompletedWonderStages()).toEqual([{vps: 3}, {vps: 5}]);
+    expect(player.getCompletedWonderStages()).toEqual(
+      [Helpers.victoryCard(3), Helpers.victoryCard(5)]
+    );
   });
 });
