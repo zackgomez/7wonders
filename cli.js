ccf6092b40f3c3f7bb4eaad1f5d0641502090897
Zack Gomez <zack.gomez@gmail.com>
2013-11-05 01:09:42 -0500

    rm user stuff in prep for merging ians stuff

--- cli.js
+++ cli.js
@@ -22,38 +22,6 @@ var bot_play_func = function (player, request) {
   }
 };
 
-/*
-var prompt_user = function(prompt, callback) {
-  console.log(prompt);
-  var input = fs.readFileSync('/dev/stdin', 'utf-8').toString();
-  return callback(input);
-};
-
-var get_player_prompt = function (player) {
-  str = '\n' + player.name + ' current hand:\n';
-  _.each(player.current_hand, function(card, index) {
-    num_spaces = 20 - card.name.length;
-    str += '\t' + card.name;
-    _(num_spaces).times(
-      function() {
-        str += ' ';
-      }
-    );
-    str +=' [' + index + ']' + '\n';
-  });
-  str += 'Which card would you like to play?\n';
-  return str;
-};
-
-var user_play_func = function (player, cards) {
-  invariant(cards.length > 0, 'must have some cards to pick from!');
-  var card = prompt_user(get_player_prompt(player), function(card) {
-    return card;
-  });
-  return Actions.play(parseInt(card.slice(0,1)));
-};
-*/
-
 var nplayers = 4;
 var game = Game.createWithNPlayers(nplayers);
 var players = game.players;
